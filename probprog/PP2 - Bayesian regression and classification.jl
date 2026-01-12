51997ed0d097a46bdf7a5a20798278f9d7bd5917
Fons van der Plas <fonsvdplas@gmail.com>
2026-01-12 12:16:04 +0100

    Fix download issue in PP lectures (#282)

--- probprog/PP2 - Bayesian regression and classification.jl
+++ probprog/PP2 - Bayesian regression and classification.jl
@@ -467,7 +467,7 @@ function get_data_file(name::String)
 		p
 	else
 		# ...otherwise download it from our github repo
-		Downloads.download("https://raw.githubusercontent.com/bmlip/course/refs/tags/v4/probprog/data/$(name)")
+		download("https://raw.githubusercontent.com/bmlip/course/refs/tags/v4/probprog/data/$(name)")
 	end
 end
 
